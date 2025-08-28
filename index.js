63fa9fe94eda19c2eebec5f5926786ce1867665b
impleotv <50145023+impleotv@users.noreply.github.com>
2025-08-28 16:39:48 +0300

    dev

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ let DATA = {
   date: new Date().toLocaleDateString('en-IL', {
     weekday: 'short',
     month: 'short',
-    year: 'numeric',
     day: 'numeric', 
+    year: 'numeric',
     hour: 'numeric',
     minute: 'numeric',
     timeZoneName: 'short',
